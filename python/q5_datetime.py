1db8021ac101de9b5403380348b4d66a4ad0edb1
Paul Black <paul.laifu.black@gmail.com>
2017-03-29 12:24:18 -0500

    Complete

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,28 @@
 # Hint:  use Google to find python function
 
-####a) 
+from datetime import date
+#function to subtract time and return answer in readable form
+def calc_days(start, stop):
+    return("There are " + str((stop - start).days) + " days between these dates.")
+####a)
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
-
+date_stop = '07-28-2015' 
+#reassign date in code readable form
+date_start = date(2013, 1, 2)
+date_stop = date(2015, 7, 28)
+print(calc_days(date_start, date_stop))
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
-
-####c)  
+date_stop = '05282015'
+#reassign date in code readable form
+date_start = date(2013,12,31)
+date_stop = date(2015,5,28)
+print(calc_days(date_start, date_stop))
+####c)
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+#reassign date in code readable form
+date_start = date(1994, 1, 15)
+date_stop = date(2015, 7, 14)
+print(calc_days(date_start, date_stop))
+ 
